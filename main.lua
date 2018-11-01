706de260bfb866350504315c751f1c2b5f1497b7
Ben <ben.erkle@gmail.com>
2018-11-01 18:57:00 +0800

    Made a couple of changes to menu position

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ local app_game = require( "app_game" )
 local widget = require('widget')
 local dropdown = require('dropdown')
 local screen = require('screen')
+local app_layout = require( "app_layout" )
 --[[
 app_io.initDatabase()
 local scenarios = app_io.getScenarios( 0 )
@@ -17,51 +18,55 @@ if ( #scenarios > 0 ) then
 end
 ]]
 
+
 app_io.printScenario( app_io.getCurrentScenario() )
 app_game.buildGame()
 
 local myDropdown
+local menuArea = app_layout.menuArea
 
 local dropdownOptions = {
   {
     title     = 'Play',
-    action    = function() 
-  
-    end 
+    action    = function()
+
+    end
   },
   {
     title     = 'Editor',
-    action    = function() 
-  
-    end 
+    action    = function()
+
+    end
   },
   {
     title     = 'Help',
-    action    = function() 
+    action    = function()
       native.showAlert('Help', 'What is it\nThe Privacy Game is a fun way to help you understand and protect your privacy online.\n\nHow does it work?\nOn the screen you will see squares with text representing different things related to your privacy online. The goal is to match the correct images to the different squares. You will begin working with an easy scenario, and the difficulty will increase as you progress through the levels. There is a scoring system, where if you happen to answer incorrectly, points will be deducted from your overall score.\n\nHow does this benefit you?\nBy matching pictures to the different labels, it is reinforcing the different scenarios you may be faced with online. It helps you identify what these potential harms may be, and then how to deal with them in an easy and understandable way. As the difficulty increases, you will learn to deal with more complex scenarios, applicable to real and tangible harms in everyday life. You will learn about the legislation and other legal instruments that are designed to protect you when using the internet.', {'Ok'})
-    end 
+    end
   },
   {
     title     = 'About',
-    action    = function() 
+    action    = function()
       native.showAlert('About Us', 'Coming Soon.', {'Ok'})
-    end 
+    end
   },
   {
     title     = 'Contact Us',
-    action    = function() 
+    action    = function()
       native.showAlert('Contact Us', 'Email', {'Ok'})
-    end 
+    end
   },
 }
 
 local button = widget.newButton{
-  width       = 56,
+  width       = 32,
   height      = 32,
-  defaultFile = 'resources/img/menu.png',
-  overFile    = 'resources/img/menu.png',
-  label = "Menu",
-  labelColor = { default={ 102, 255, 0, 255 }, over={ 164, 198, 57} },
+  --x           = menuArea.xMin + (menuArea.width/2),
+  --y           = menuArea.yMin,
+  defaultFile = 'resources/img/menu_white.png',
+  overFile    = 'resources/img/menu_white.png',
+  --label = "Menu",
+  --labelColor = { default={ 102, 255, 0, 255 }, over={ 164, 198, 57} },
   onEvent     = function( event )
     local target = event.target
     local phase  = event.phase
@@ -75,11 +80,12 @@ local button = widget.newButton{
     end
   end
 }
-button.alpha = .5
+--button.alpha = .5
+button.anchorY = 0
 
 myDropdown     = dropdown.new{
-  x            = screen.leftSide + 40,
-  y            = screen.topSide + 50,
+  x            = menuArea.xMin + (menuArea.width/2),
+  y            = menuArea.yMin,
   toggleButton = button,
 
   width        = 280,
@@ -87,4 +93,3 @@ myDropdown     = dropdown.new{
   padding      = 10,
   options      = dropdownOptions
 }
-
